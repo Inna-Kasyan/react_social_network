8cb198c4738c1ac4176a082db956e14ade1e672e
Inna Kasyan <777inness@mail.ru>
2022-12-28 22:09:55 +0300

    lesson_62

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 // import Music from './components/Music/Music';
@@ -10,6 +9,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 
 function App(props) {
@@ -17,7 +17,7 @@ function App(props) {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
-        <Header />
+        <HeaderContainer />
         <Navbar state={props.state} />
         <div className="app-wrapper-content">
           <Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css'
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={classes.header}>
         <img src='https://www.logodesign.net/logo/letter-d-inside-polygon-4839ld.png'></img>
+        <div className={classes.loginBlock}>
+          { props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
       </header>
     )
 }
